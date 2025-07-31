fa8dda657f21ceea09d382fc9db46c7633badd78
jinghaihan <jinghaihan@e-byte.com>
2025-07-31 12:59:06 +0800

    chore: update deps

--- pncat.config.ts
+++ pncat.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig, mergeCatalogRules } from 'pncat'
 
 export default defineConfig({
-  ignorePaths: ['playground'],
+  ignorePaths: [
+    'playground',
+  ],
   catalogRules: mergeCatalogRules([
     {
-      name: 'vue-plugin',
+      name: 'vue',
       match: [/plugin-vue/, 'unplugin-auto-import'],
       priority: 0,
     },
     {
-      name: 'common-plugin',
+      name: 'common',
       match: [/vite-plugin/, /rollup-plugin/],
       priority: 0,
     },
